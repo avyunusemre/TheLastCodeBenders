91ff19cc6e99ee86b3a7f33b2c736976eff86d0a
avyunusemre <67027810+avyunusemre@users.noreply.github.com>
2022-11-14 18:07:43 +0100

    Update FirstClass.java

--- src/interviewQuestions/FirstClass.java
+++ src/interviewQuestions/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Merhaba Dünya");
+        System.out.println("Hallo Welt");
     }
 }
